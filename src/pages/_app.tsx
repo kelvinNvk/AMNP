0391d6ebb9acfad4f74a91121adecd1f3c540ae4
Kelvin Eduardo <kelvin@nibbletecnologia.com.br>
2022-01-18 13:05:59 -0300

    second part h

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
-import { QueryClientProvider } from 'react-query';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
@@ -10,14 +11,18 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
         <Component {...pageProps} />
       </SidebarDrawerProvider>
     </ChakraProvider>
+
+    <ReactQueryDevtools />
   </QueryClientProvider>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,28 +1,14 @@
 import { Box, Flex, Button, Heading, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-      const response = await fetch('http://localhost:3000/api/users')
-      const data = await response.json()
-
-      const users = data.users.map(user => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString()
-        };
-      });
-
-      return users;
-  })
+  const { data, isLoading, isFetching, error } = useUsers()
 
   console.log()
 
@@ -39,7 +25,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" /> }
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
